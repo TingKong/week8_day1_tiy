c3b643ebbf4f7acb473798c03997a7467043a4d6
TingKong <kongsau@gmail.com>
2016-11-14 16:08:52 -0500

    uploading files

---
+++ first_api/first_api/Controllers/SportsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using First_Api.Models;
+
+namespace First_Api.Controllers
+{
+    public class SportsController : Controller
+    {
+        // GET: Sports
+        Sports[] sports = new Sports[]
+        {
+            new Sports { Id = 1, Name = "Swimming", Seasons = "Summer" },
+            new Sports { Id = 2, Name = "VolleyBall", Seasons = "Summer"},
+            new Sports { Id = 3, Name = "Snow Boarding", Seasons = "Winter"},
+            new Sports { Id = 4, Name = "Football", Seasons = "Fall"}
+        };
+
+        public IEnumerable<Sports> GetAllProducts()
+        {
+            return products;
+        }
+
+    }
+}
