c387439ac5a2aa15d30c23bd1cff4b3bf351197e
TingKong <kongsau@gmail.com>
2016-11-15 14:29:16 -0500

    adding list item

--- first_api/first_api/Controllers/SportsController.cs
+++ first_api/first_api/Controllers/SportsController.cs
@@ -1,27 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using First_Api.Models;
 
 namespace First_Api.Controllers
 {
-    public class SportsController : Controller
+    public class SportsController : ApiController
     {
         // GET: Sports
-        Sports[] sports = new Sports[]
+        List<Sports> sports = new List<Sports>
+
         {
-            new Sports { Id = 1, Name = "Swimming", Seasons = "Summer" },
-            new Sports { Id = 2, Name = "VolleyBall", Seasons = "Summer"},
-            new Sports { Id = 3, Name = "Snow Boarding", Seasons = "Winter"},
-            new Sports { Id = 4, Name = "Football", Seasons = "Fall"}
+            new Sports { Id = 1, Name = "Swimming", Seasons = "Summer", Players = 1 },
+            new Sports { Id = 2, Name = "VolleyBall", Seasons = "Spring", Players = 6},
+            new Sports { Id = 3, Name = "Snow Boarding", Seasons = "Winter", Players = 1},
+            new Sports { Id = 4, Name = "Football", Seasons = "Fall", Players = 11}
         };
 
-        public IEnumerable<Sports> GetAllProducts()
+        public IEnumerable<Sports> GetAllSports()
         {
-            return products;
+            return sports;
         }
+        //IEnumerable<Sports> 
+        public IHttpActionResult  GetSports(int id)
+        {
+          
+            var sport = sports.SingleOrDefault(p => p.Id == id);
+
+            if (sport == null)
+                return NotFound();
+
+            return Ok(sport);
+        }
+
+
+        public IHttpActionResult GetSportBySeason(string season)
+        {
+            //var product = from p in products
+            //              where p.Category.ToLower() == cat.ToLower();
+            //select p;
+            var sport = sports.SingleOrDefault(p => p.Seasons.ToLower() == season.ToLower());
+
+            if (sport == null)
+                return NotFound();
+
+            return Ok(sport);
+        }
+
+        public IEnumerable<Sports> GetoList(int id, string sportse, string season, int players)
+        {
+            new Sports { Id = 1, Name = "Swimming", Seasons = "Summer", Players = 1 };
+
+            Sports newSports = new Models.Sports();
+            newSports.Id = id;
+            newSports.Name = sportse;
+            newSports.Seasons = season;
+            newSports.Players = players;
+
+            sports.Add(newSports);
+
+            return sports;
+        }
+
 
     }
 }
--- first_api/first_api/Models/sports.cs
+++ first_api/first_api/Models/sports.cs
@@ -11,6 +11,7 @@ namespace First_Api.Models
             public int Id { get; set; }
             public string Name { get; set; }
             public string Seasons { get; set; }
+            public int Players { get; set; }
         
     }
 }
